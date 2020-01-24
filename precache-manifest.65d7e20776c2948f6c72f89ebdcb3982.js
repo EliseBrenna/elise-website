42081dd9697e938bd5d41790e2af008a6aeadac6
superelise <57939154+EliseBrenna@users.noreply.github.com>
2020-01-24 10:10:06 +0100

    Updates

--- precache-manifest.65d7e20776c2948f6c72f89ebdcb3982.js
+++ precache-manifest.65d7e20776c2948f6c72f89ebdcb3982.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "14560b9b28fcfa031611bad12ebcac91",
+    "revision": "3315971bf24514d26594664069cae77b",
     "url": "/elise-website/index.html"
   },
   {
-    "revision": "31fba204cf27cca1e497",
-    "url": "/elise-website/static/css/main.d9aa2b00.chunk.css"
+    "revision": "9483d1a1112403d75d22",
+    "url": "/elise-website/static/css/main.3431cb8a.chunk.css"
   },
   {
     "revision": "1c7422d8b3bc0eb67126",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/elise-website/static/js/2.c401c973.chunk.js.LICENSE"
   },
   {
-    "revision": "31fba204cf27cca1e497",
-    "url": "/elise-website/static/js/main.1ae338b3.chunk.js"
+    "revision": "9483d1a1112403d75d22",
+    "url": "/elise-website/static/js/main.69edc351.chunk.js"
   },
   {
     "revision": "72227622ef94ba082753",
